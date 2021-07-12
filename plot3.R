6de00fa3557a963e3730023801f688e097850460
Abid Raza <45925346+AbidRaza1@users.noreply.github.com>
2021-07-12 14:23:50 -0600

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -29,10 +29,10 @@ plot(df1$Date+df1$Time, df1$Sub_metering_3, type ="l",xlab = "", ylab =  "",axes
 
 legend(x ="topright",legend = c("Sub_metering_1","Sub_metering_","Sub_metering_3"),lty =c(1,1,1),col = c("black","red","blue"))
 
-## Copying Image in .PNG format
+# Copying Image in .PNG format
 
 dev.copy(png,"plot3.png",width= 480, height=480)
 
-## Complete saving
+# Complete saving
 
 dev.off()
