3fb0e35709e4fa5bee9a01ae3ab9eff949d737c8
Abid Raza <45925346+AbidRaza1@users.noreply.github.com>
2021-07-12 14:16:05 -0600

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,10 +1,14 @@
 
 ##--------------Plotting distribution of Global Active Power------------------
 
+## Converting data tyoe of the dataset
 x<-as.numeric(as.character(df1$Global_active_power))
 
+## Plotting Histogram 
 hist(x, xlab = "Global Active Power(kilowatts)" ,main = "Global Active Power",col = "red")
 
+## Copying Image in .PNG format
 dev.copy(png,"plot1.png",width= 480, height=480)
 
+## Complete saving
 dev.off()
