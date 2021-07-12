a60c0ce8a2174bea0cd21a5615960fc7ac52f928
Abid Raza <45925346+AbidRaza1@users.noreply.github.com>
2021-07-12 14:22:41 -0600

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -3,21 +3,39 @@
 
 
 
-#-------------Plotting Energy Sub metering over the period of two days----------
+#-------------Plotting Energy Sub metering over the period of two days------------#
 
+
+# Plotting line plot
 
 plot(df1$Date+df1$Time, df1$Sub_metering_1, type ="l",xlab = "", ylab =  "Energy Sub metering", col =c("black"))
 
+# Adding new screen 
+
 par(new=TRUE)
+
+# Plotting line plot
 
 plot(df1$Date+df1$Time, df1$Sub_metering_2, type ="l",xlab = "", ylab =  "",axes=FALSE, col =c("red"))
 
+# Adding new screen 
+
 par(new=TRUE)
+
+# Plotting line plot
+
+plot(df1$Date+df1$Time, df1)
 
 plot(df1$Date+df1$Time, df1$Sub_metering_3, type ="l",xlab = "", ylab =  "",axes=FALSE, col =c("blue"))
 
+# Adding legends to the plot     
+
 legend(x ="topright",legend = c("Sub_metering_1","Sub_metering_","Sub_metering_3"),lty =c(1,1,1),col = c("black","red","blue"))
 
+## Copying Image in .PNG format
+
 dev.copy(png,"plot3.png",width= 480, height=480)
+
+## Complete saving
 
 dev.off()
