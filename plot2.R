d7723f04f612c1b307fcbd76e8fddda35f636fb9
Abid Raza <45925346+AbidRaza1@users.noreply.github.com>
2021-07-12 14:29:22 -0600

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -3,11 +3,14 @@
                                          ##-------------Plotting Global Active Power(kw) over the period of two days-----------------##
 
 
-## Plotting Scatter plot
+# Plotting Scatter plot
+
 plot(df1$Date+df1$Time, df1$Global_active_power, type ="l",xlab = "", ylab =  "Global Active Power(kilowatts)")
 
-## Copying Image in .PNG format
+# Copying Image in .PNG format
+
 dev.copy(png,"plot2.png",width= 480, height=480)
 
-## Complete saving
+# Complete saving
+
 dev.off()
